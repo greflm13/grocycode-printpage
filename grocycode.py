b9b0466dc27aa95d1b26c3ebddfcc8d8d9398a9d
Flo Greistorfer <flo.greistorfer@sorogon.eu>
2026-04-17 09:14:02 +0200

    added support to get id from api

--- grocycode.py
+++ grocycode.py
@@ -1,8 +1,11 @@
 #!/bin/env python
 import os
 import io
+import re
+import json
 import argparse
 
+import requests
 
 from PIL import Image
 from pylibdmtx.pylibdmtx import encode
@@ -15,12 +18,44 @@ from reportlab.pdfbase.ttfonts import TTFont
 SCRIPTDIR = os.path.dirname(os.path.realpath(__file__)).removesuffix(__package__ if __package__ else "")
 Image.MAX_IMAGE_PIXELS = 9331200000
 
+BASE_URL_RE = re.compile(r"^https?:\/\/((([A-Za-z0-9-]+\.)+[A-Za-z]{2,})|localhost|(\d{1,3}\.){3}\d{1,3})(:\d+)?$")
+
 pdfmetrics.registerFont(TTFont("header", "FiraSans-Black.ttf"))
+
+
+def base_url_or_int_type(value: str) -> tuple[str, str]:
+    if BASE_URL_RE.match(value):
+        return ("url", value)
+
+    try:
+        int(value)
+        return ("id", value)
+    except ValueError:
+        raise argparse.ArgumentTypeError("Must be either a base URL (http[s]://host[:port]) or an integer")
+
+
+def check_or_load_login() -> str:
+    file = os.path.join(SCRIPTDIR, ".api_key")
+    if not os.path.exists(file):
+        api_key = input("Your api key: ")
+        login_dict = {"api_key": api_key}
+
+        persist = input("Do you want to save your api key to a hidden file?: [y/n] ")
+        if persist.lower() in {"yes", "y", "ye", "j", "ja"}:
+            with open(file, "w") as login_file:
+                json.dump(login_dict, login_file)
+            print("Your password has been saved to <" + str(file) + ">, to renew it please delete the file\n")
+
+    else:
+        with open(file, "r") as login_file:
+            login_dict = json.load(login_file)
+            api_key = login_dict["api_key"]
+    return api_key
 
 
 def argparser() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="create printable pdf of grocycode")
-    parser.add_argument("product_id", help="product id for the code", type=str)
+    parser.add_argument("product_id", help="product id for the code", type=base_url_or_int_type)
     parser.add_argument("product_name", help="product name for the code", type=str)
     parser.add_argument("-o", "--output", help="output directory (default 'output')", type=str, default="output")
     return parser.parse_args()
@@ -42,7 +77,21 @@ def main() -> None:
     pdf.setFillColor(black)
     pdf.drawCentredString(x=2100 / 2, y=2870, text=args.product_name)
 
-    encoded = encode(f"grcy:p:{args.product_id}".encode("utf-8"))
+    typ, value = args.product_id
+    if typ == "id":
+        product_id = value
+    elif typ == "url":
+        api_key = check_or_load_login()
+        res = requests.get(
+            url=f"{value}/api/objects/products?query%5B%5D=name%3D{args.product_name}",
+            headers={"accept": "application/json", "GROCY-API-KEY": api_key},
+        )
+        try:
+            product_id = res.json()[0]["id"]
+        except IndexError:
+            raise RuntimeError("Product not found: %s" % args.product_name)
+
+    encoded = encode(f"grcy:p:{product_id}".encode("utf-8"))
     img = Image.frombytes("RGB", (encoded.width, encoded.height), encoded.pixels)
     resized = img.resize((900, 900), resample=Image.Resampling.NEAREST)
 
